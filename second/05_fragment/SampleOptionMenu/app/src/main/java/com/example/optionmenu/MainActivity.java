eb086b4957f1a75dd33911887b87dc8d63831f7b
jjuyaa <smwujjuyaa@gmail.com>
2021-07-27 16:36:30 +0900

    [Doit] 둘째마당 05

--- second/05_fragment/SampleOptionMenu/app/src/main/java/com/example/optionmenu/MainActivity.java
+++ second/05_fragment/SampleOptionMenu/app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.optionmenu;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int curId = item.getItemId();
+        switch (curId) {
+            case R.id.menu_refresh:
+                Toast.makeText(this, "새로고침 메뉴가 선택되었습니다. ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_search:
+                Toast.makeText(this, "검색 메뉴가 선택되었습니다. ", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_settings:
+                Toast.makeText(this, "설정 메뉴가 선택되었습니다. ", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
