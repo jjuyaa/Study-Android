2e7345333f14b19d6d80abc07ebd6ca9be7affad
jjuyaa <smwujjuyaa@gmail.com>
2021-08-28 10:32:10 +0900

    [Doit] 둘째마당 07 Mission

---
+++ second/07_select/Mission/Mission13/app/src/main/java/com/example/mission13/OnCustomerItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.mission13;
+
+import android.view.View;
+
+public interface OnCustomerItemClickListener {
+    public void onItemClick(CustomerAdapter.ViewHolder holder, View view, int position);
+}
